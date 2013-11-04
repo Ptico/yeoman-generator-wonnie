858f24b68af75dc0e7ac1755605951e392e3259b
Andrey Savchenko <andrey@aejis.eu>
2013-11-04 18:35:20 +0200

    Fix requirejs task

--- app/templates/grunt_config/requirejs-multipage.js
+++ app/templates/grunt_config/requirejs-multipage.js
@@ -10,7 +10,7 @@ var extend  = require('util')._extend,
 
 // !! Place your files here !! //
 //    ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼    //
-modules = ['application'];
+modules = ['app/main'];
 
 // Basic options
 options = {
